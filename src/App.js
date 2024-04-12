b4d75d3720a4701184d492f50983cc1cce028c29
saivyshanavi <saivyshanavitulasi@gmail.com>
2024-04-12 13:26:57 +0530

    solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter} from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import './App.css'
+import Sidebar from './components/Sidebar'
+import Footer from './components/Footer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <BrowserRouter>
+    <div className="sidebar-and-content-container">
+      <Sidebar />
+      <div>
+        <Home />
+        <About />
+        <Contact />
+      </div>
     </div>
-  );
-}
+    <Footer />
+  </BrowserRouter>
+)
 
-export default App;
+export default App
